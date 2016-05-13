0054677e3d15ca034229cfeeb1ee33cddf790c9a
Ron Yu <ronyu77@gmail.com>
2016-05-14 00:36:19 +0800

    show all images

--- build/main.js
+++ build/main.js
@@ -1,5 +1,76 @@
 'use strict';
 
-var name = 'Ron';
+window.onload = function () {};
 
-console.log('hello ' + name);
+var CONTAINER = document.getElementById('container');
+var SQUARE = [].slice.call(document.querySelectorAll('.square'));
+var SQUARE_LENGTH = SQUARE.length;
+
+// set original array
+var originalArr = [];
+for (var i = 0; i < SQUARE_LENGTH; i++) {
+    originalArr[i] = i;
+} // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+// define shuffle function in array prototype
+Array.prototype.shuffle = function () {
+    return this.sort(function () {
+        return .5 - Math.random();
+    });
+};
+
+// get shuffled array
+function shuffledArr() {
+
+    var posArr = originalArr.slice().shuffle();
+    var imgArr = originalArr.slice().shuffle();
+
+    return { posArr: posArr, imgArr: imgArr };
+}
+
+// get random position and img number pairs
+function getRandomPairs() {
+    var _shuffledArr = shuffledArr();
+
+    var posArr = _shuffledArr.posArr;
+    var imgArr = _shuffledArr.imgArr;
+
+    var randomPairs = [];
+
+    posArr.forEach(function (item, i) {
+        randomPairs[i] = {};
+        randomPairs[i]['posId'] = posArr[i];
+        randomPairs[i]['imgId'] = imgArr[i];
+    });
+
+    return randomPairs;
+}
+
+// creat image
+function creatImg(id) {
+
+    var img = new Image();
+    img.src = './img/' + id + '.png';
+    img.classList.add('squareImg');
+
+    return img;
+}
+
+console.log(getRandomPairs());
+
+// insert image node into square
+function insertImg() {
+
+    var randomPairs = getRandomPairs();
+
+    randomPairs.forEach(function (item, i) {
+
+        var squareNode = document.getElementById(item.posId);
+        var imageNode = creatImg(item.imgId);
+
+        squareNode.appendChild(imageNode);
+
+        console.log(squareNode, imageNode);
+    });
+}
+console.log(insertImg());
--- src/main.js
+++ src/main.js
@@ -1,3 +1,77 @@
-const name = 'Ron';
+window.onload = function() {
 
-console.log(`hello ${name}`);
+};
+
+const CONTAINER = document.getElementById('container');
+const SQUARE = [].slice.call(document.querySelectorAll('.square'));
+const SQUARE_LENGTH = SQUARE.length;
+
+// set original array
+let originalArr = [];
+for (let i = 0; i < SQUARE_LENGTH; i++) {
+    originalArr[i] = i;
+} // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+// define shuffle function in array prototype
+Array.prototype.shuffle = function() {
+    return this.sort(function() {
+        return .5 - Math.random();
+    });
+}
+
+
+// get shuffled array
+function shuffledArr() {
+
+    let posArr = originalArr.slice().shuffle();
+    let imgArr = originalArr.slice().shuffle();
+
+    return {posArr, imgArr}
+}
+
+
+// get random position and img number pairs
+function getRandomPairs() {
+
+    let {posArr, imgArr} = shuffledArr();
+    let randomPairs = [];
+
+    posArr.forEach(function(item, i) {
+        randomPairs[i] = {};
+        randomPairs[i]['posId'] = posArr[i];
+        randomPairs[i]['imgId'] = imgArr[i];
+    });
+
+    return randomPairs;
+}
+
+
+// creat image
+function creatImg(id) {
+
+    let img = new Image();
+    img.src = `./img/${id}.png`;
+    img.classList.add('squareImg');
+
+    return img;
+}
+
+console.log(getRandomPairs())
+
+// insert image node into square
+function insertImg() {
+
+    let randomPairs = getRandomPairs();
+
+    randomPairs.forEach(function(item , i) {
+
+        let squareNode = document.getElementById(item.posId);
+        let imageNode = creatImg(item.imgId);
+
+        squareNode.appendChild(imageNode);
+
+        console.log(squareNode, imageNode);
+    })
+}
+console.log(insertImg())
