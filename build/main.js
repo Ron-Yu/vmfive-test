f88548277e17615328a79f49b037068bcc5bb95e
Ron Yu <ronyu77@gmail.com>
2016-05-15 17:16:58 +0800

    modify dnd function

--- build/main.js
+++ build/main.js
@@ -92,25 +92,17 @@ Array.prototype.forEach.call(squareImgs, function (squareImg, i) {
 
     squareImg.addEventListener('touchstart', function (event) {
         event.preventDefault();
-        console.log(event.changedTouches[0].pageX);
-        console.log(event.changedTouches[0].pageY);
-        console.log(squareImg.scrollTop);
-        // squareImg.style.position = 'fixed'
-        console.log(squareImg.style);
-        console.log(squareImg.getBoundingClientRect().left, squareImg.getBoundingClientRect().top);
-        // this.style.left = `${event.changedTouches[0].pageX}px`
-        // this.style.top = `${event.changedTouches[0].pageY}px`
+
         this.style.zIndex = 2;
     }, false);
 
     squareImg.addEventListener('touchmove', function (event) {
         event.preventDefault();
-        console.log(event.changedTouches[0].pageX);
-        console.log(event.changedTouches[0].pageY);
-        // console.log(squareImg.getBoundingClientRect().top, squareImg.getBoundingClientRect().left)
-        // this.style.left = `${event.changedTouches[0].pageX}px`
-        // this.style.top = `${event.changedTouches[0].pageY}px`
-        this.style.zIndex = 2;
+        this.classList.add('dragging');
+        this.style.top = event.changedTouches[0].pageY - (screenHeight * 0.5 - screenWidth * 0.3) + 'px';
+        this.style.left = event.changedTouches[0].pageX - screenWidth * 0.1 + 'px';
+        console.log(this.style.top, this.style.left);
+        console.log(event.changedTouches[0].pageX, event.changedTouches[0].pageY);
     }, false);
 
     squareImg.addEventListener('touchend', function (event) {
@@ -131,12 +123,12 @@ Array.prototype.forEach.call(squareImgs, function (squareImg, i) {
 
         var targetImg = event.target;
         var targetPos = targetImg.getBoundingClientRect();
-        // console.log(targetPos)
         var targetImgSrc = targetImg.getAttribute('src');
         var desPosSquare = document.getElementById('square' + desPosSquareNum);
         var desPosImg = desPosSquare.querySelector('.squareImg');
         var desImgSrc = desPosImg.getAttribute('src');
 
+        targetImg.classList.remove('dragging');
         targetImg.style.left = squareWidth / 2 + 'px';
         targetImg.style.top = squareWidth / 2 + 'px';
         targetImg.style.zIndex = 1;
--- src/main.js
+++ src/main.js
@@ -97,26 +97,18 @@ Array.prototype.forEach.call(squareImgs ,function(squareImg, i) {
 
     squareImg.addEventListener('touchstart', function(event) {
         event.preventDefault();
-        console.log(event.changedTouches[0].pageX)
-        console.log(event.changedTouches[0].pageY)
-        console.log(squareImg.scrollTop)
-        // squareImg.style.position = 'fixed'
-        console.log(squareImg.style)
-        console.log(squareImg.getBoundingClientRect().left, squareImg.getBoundingClientRect().top)
-        // this.style.left = `${event.changedTouches[0].pageX}px`
-        // this.style.top = `${event.changedTouches[0].pageY}px`
+
         this.style.zIndex = 2;
 
     }, false);
 
     squareImg.addEventListener('touchmove', function(event) {
         event.preventDefault();
-        console.log(event.changedTouches[0].pageX)
-        console.log(event.changedTouches[0].pageY)
-        // console.log(squareImg.getBoundingClientRect().top, squareImg.getBoundingClientRect().left)
-        // this.style.left = `${event.changedTouches[0].pageX}px`
-        // this.style.top = `${event.changedTouches[0].pageY}px`
-        this.style.zIndex = 2;
+        this.classList.add('dragging');
+        this.style.top = `${event.changedTouches[0].pageY - (screenHeight * 0.5 - screenWidth * 0.3)}px`
+        this.style.left = `${event.changedTouches[0].pageX - screenWidth * 0.1}px`;
+        console.log(this.style.top, this.style.left)
+        console.log(event.changedTouches[0].pageX, event.changedTouches[0].pageY)
 
     }, false);
 
@@ -138,13 +130,12 @@ Array.prototype.forEach.call(squareImgs ,function(squareImg, i) {
 
         let targetImg = event.target;
         let targetPos = targetImg.getBoundingClientRect();
-        // console.log(targetPos)
         let targetImgSrc = targetImg.getAttribute('src');
         let desPosSquare = document.getElementById(`square${desPosSquareNum}`);
         let desPosImg = desPosSquare.querySelector('.squareImg')
         let desImgSrc = desPosImg.getAttribute('src');
 
-
+        targetImg.classList.remove('dragging');
         targetImg.style.left = squareWidth/2 + 'px';
         targetImg.style.top = squareWidth/2 + 'px';
         targetImg.style.zIndex = 1;
